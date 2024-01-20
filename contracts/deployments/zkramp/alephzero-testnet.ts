4b718d29d0abb5a3f10fe729a8422161952c6af4
Glaicon <gjpeixer@gmail.com>
2024-01-19 20:42:13 -0500

    update deployment file

--- contracts/deployments/zkramp/alephzero-testnet.ts
+++ contracts/deployments/zkramp/alephzero-testnet.ts
@@ -1,2 +1,2 @@
-export const address = '5DBey8BEPR4AFoC6SCGyaW5BHHMch1bKoC1bqWGzqMRo5hFE'
-export const blockNumber = 52230407
+export const address = '5GbSCJukgyAkVeYdTTDiw4mbTFQP9wKPQ5WhDgHf7NyY9QYA'
+export const blockNumber = 52340496
