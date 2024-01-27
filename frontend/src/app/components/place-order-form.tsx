7431a6761e75d2fd6424d490c630efee5c37bbd3
Glaicon <gjpeixer@gmail.com>
2024-01-27 18:45:55 -0500

    Add create order page

--- frontend/src/app/components/place-order-form.tsx
+++ frontend/src/app/components/place-order-form.tsx
@@ -1,57 +1,59 @@
+import { useState } from 'react'
+
 import { XCircleIcon } from '@heroicons/react/24/outline'
 
 import { Button } from '../../components/ui/button'
 import { Input } from '../../components/ui/input'
 
-export type PaymentInfo =
-  | {
-      type: 'PIX'
-      key: string
-    }
-  | {
-      type: 'WISE'
-      wisetag: string
-    }
+const FormInput = ({
+  title,
+  subtitle,
+  placeholder,
+  value,
+  setValue,
+}: {
+  title: string
+  subtitle?: string
+  placeholder: string
+  value: string
+  setValue: (value: string) => void
+}) => {
+  return (
+    <div className="inline-flex w-full flex-col items-start justify-start gap-1 rounded border border-zinc-800 bg-zinc-900 px-4 py-2">
+      <div className="inline-flex items-center justify-between gap-3 self-stretch">
+        <div className="shrink grow basis-0 font-manrope text-base font-medium leading-normal text-zinc-400">
+          {title}
+        </div>
+      </div>
+      {!!subtitle && (
+        <div className="h-5 font-manrope text-sm font-normal leading-tight text-zinc-500">
+          {subtitle}
+        </div>
+      )}
+      <div className="inline-flex items-center justify-start gap-1 self-stretch px-1.5 py-2">
+        <Input
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value)
+          }}
+          className="shrink grow basis-0 font-manrope text-base font-normal leading-tight text-zinc-500"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default function PlaceOrderForm({
   onSubmit,
   onClose,
 }: {
-  onSubmit: (paymentInfo: PaymentInfo) => void
+  onSubmit: (order: any) => void
   onClose: () => void
 }) {
-  // useStates here
-
-  const FormInput = ({
-    title,
-    subtitle,
-    placeholder,
-  }: {
-    title: string
-    subtitle?: string
-    placeholder: string
-  }) => {
-    return (
-      <div className="inline-flex w-full flex-col items-start justify-start gap-1 rounded border border-zinc-800 bg-zinc-900 px-4 py-2">
-        <div className="inline-flex items-center justify-between gap-3 self-stretch">
-          <div className="shrink grow basis-0 font-manrope text-base font-medium leading-normal text-zinc-400">
-            {title}
-          </div>
-        </div>
-        {!!subtitle && (
-          <div className="h-5 font-manrope text-sm font-normal leading-tight text-zinc-500">
-            {subtitle}
-          </div>
-        )}
-        <div className="inline-flex items-center justify-start gap-1 self-stretch px-1.5 py-2">
-          <Input
-            placeholder={placeholder}
-            className="shrink grow basis-0 font-manrope text-base font-normal leading-tight text-zinc-500"
-          />
-        </div>
-      </div>
-    )
-  }
+  const [paymentKey, setPaymentKey] = useState('')
+  const [depositAmount, setDepositAmount] = useState('')
+  const [receiveAmount, setReceiveAmount] = useState('')
 
   return (
     <div className="inline-flex flex-col items-start justify-center gap-4 rounded border border-zinc-800 bg-zinc-950 p-4">
@@ -73,18 +75,39 @@ export default function PlaceOrderForm({
         <div className="inline-flex items-center justify-start gap-1 self-stretch px-1.5 py-2">
           <Input
             placeholder="Wisetag"
+            value={paymentKey}
+            onChange={(e) => {
+              setPaymentKey(e.target.value)
+            }}
             className="shrink grow basis-0 font-manrope text-base font-normal leading-tight text-zinc-500"
           />
         </div>
       </div>
       <FormInput
-        title="Identify yourself"
-        subtitle="Don't worry, this information is private"
-        placeholder="Your name"
+        title="Deposit Amount"
+        value={depositAmount}
+        setValue={setDepositAmount}
+        subtitle="Colateral description here"
+        placeholder="0"
       />
-      <FormInput title="Deposit Amount" subtitle="Colateral description here" placeholder="0" />
-      <FormInput title="Exchange" placeholder="0" />
-      <Button className="w-full">Place Order</Button>
+      <FormInput
+        title="Receive Amount"
+        value={receiveAmount}
+        setValue={setReceiveAmount}
+        placeholder="0"
+      />
+      <Button
+        className="w-full"
+        onClick={() =>
+          onSubmit({
+            paymentKey,
+            depositAmount,
+            receiveAmount,
+          })
+        }
+      >
+        Place Order
+      </Button>
     </div>
   )
 }
--- frontend/src/app/orders/page.tsx
+++ frontend/src/app/orders/page.tsx
@@ -11,64 +11,16 @@ import { contractTxWithToast } from '@/utils/contract-tx-with-toast'
 
 import Modal from '../../components/ui/modal'
 import OrderTable from '../components/order-table'
-import PlaceOrderForm, { PaymentInfo } from '../components/place-order-form'
+import PlaceOrderForm from '../components/place-order-form'
 import UploadReceiptModal from '../components/upload-receipt-modal'
 
 export default function OrdersPage() {
   const [showPlaceOrderForm, setShowPlaceOrderForm] = useState(false)
   const [selectedOrderUploadReceipt, setSelectedOrderUploadReceipt] = useState<any>(null)
-
-  const EmptyDepositsTitle = () => {
-    return (
-      <div className="mt-16 flex flex-col items-center gap-1 md:mt-0">
-        <h1 className="text-2xl font-extrabold leading-loose text-white">Orders</h1>
-        <h2 className="text-base font-normal leading-normal text-zinc-300">No deposits found</h2>
-      </div>
-    )
-  }
-
-  const NewDepositsTitle = () => {
-    return (
-      <div className="flex flex-col items-center gap-1">
-        <h1 className="text-2xl font-extrabold leading-loose text-white">New Order</h1>
-        <h2 className="text-base font-normal leading-normal text-zinc-300">
-          Placing a new order in the pool
-        </h2>
-      </div>
-    )
-  }
+  const { api, activeAccount, activeSigner } = useInkathon()
+  const { contract } = useRegisteredContract(ContractIds.zkramp)
 
   const Title = () => {
-    const { api, activeAccount, activeSigner } = useInkathon()
-    const { contract, address: contractAddress } = useRegisteredContract(ContractIds.zkramp)
-
-    const createOrder = async () => {
-      if (!activeAccount || !contract || !activeSigner || !api) {
-        toast.error('Wallet not connected. Try again…')
-        return
-      }
-
-      const depositAmount = prompt('Enter deposit amount')
-
-      if (!depositAmount) {
-        toast.error('Deposit amount is required')
-        return
-      }
-
-      const converteddepositAmount = parseInt(depositAmount || '0', 10)
-
-      await contractTxWithToast(
-        api,
-        activeAccount.address,
-        contract,
-        'create_order',
-        {
-          value: converteddepositAmount,
-        },
-        [converteddepositAmount, 'transfer identifier', 'HASH_NAME', 1],
-      )
-    }
-
     return (
       <div className="mt-16 flex w-full items-end justify-between gap-1 md:mt-0">
         <h1 className="text-2xl font-extrabold leading-loose text-white">Orders</h1>
@@ -77,8 +29,22 @@ export default function OrdersPage() {
     )
   }
 
-  const handleSubmit = (paymentInfo: PaymentInfo) => {
-    console.log(paymentInfo)
+  const createOrder = async (order: any) => {
+    if (!activeAccount || !contract || !activeSigner || !api) {
+      toast.error('Wallet not connected. Try again…')
+      return
+    }
+
+    await contractTxWithToast(
+      api,
+      activeAccount.address,
+      contract,
+      'create_order',
+      {
+        value: order.amountDeposit * 2,
+      },
+      [order.amountReceive, order.paymentKey, 'HASH_NAME', 1],
+    )
   }
 
   return (
@@ -97,10 +63,7 @@ export default function OrdersPage() {
 
           {showPlaceOrderForm && (
             <Modal>
-              <PlaceOrderForm
-                onSubmit={handleSubmit}
-                onClose={() => setShowPlaceOrderForm(false)}
-              />
+              <PlaceOrderForm onSubmit={createOrder} onClose={() => setShowPlaceOrderForm(false)} />
             </Modal>
           )}
         </main>
