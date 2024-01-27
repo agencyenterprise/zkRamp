3cbbf864be2541c6c48237bf2183287244dd6724
Glaicon <gjpeixer@gmail.com>
2024-01-26 22:14:37 -0500

    update smart contract

--- contracts/deployments/zkramp/alephzero-testnet.ts
+++ contracts/deployments/zkramp/alephzero-testnet.ts
@@ -1,2 +1,2 @@
-export const address = '5G6KJiBkWwYourZUm5CRXo1MnPG6LQaXXM66SDzWAJx5WFSe'
-export const blockNumber = 52410560
+export const address = '5FvZdvjCAXDg8C3ETKHN5kSTjnZesQZ1n4wRTyvY3DM16MAR'
+export const blockNumber = 52942539
