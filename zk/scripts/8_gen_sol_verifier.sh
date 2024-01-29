9db563174cc5ed03fcb11e0d4f8667c53dee6d26
lolo <lolo.tomaz@gmail.com>
2024-01-29 03:25:44 -0300

    removed files for proof generation

--- zk/scripts/8_gen_sol_verifier.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source circuit.env
-
-echo "Generating solidity verifier..."
-node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey export solidityverifier "$BUILD_DIR"/"$CIRCUIT_NAME".zkey ../circuits/$CIRCUIT_DIR/contracts/"$CIRCUIT_NAME"_verifier.sol
--- zk/scripts/9_gen_sol_calldata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source circuit.env
-
-echo "Generating solidity calldata..."
-node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey export soliditycalldata "$BUILD_DIR"/rapidsnark_public.json "$BUILD_DIR"/rapidsnark_proof.json
